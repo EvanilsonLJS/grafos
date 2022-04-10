df771dd2debbd8ebce79a85061ad44eed9199aa6
EvanilsonLJS <eljs@cesar.school>
2022-04-10 16:51:40 -0300

    Add requirement 7

--- modules/main.py
+++ modules/main.py
@@ -22,11 +22,12 @@ while opcao != 0:
     print('\nDigite:')
     print("1 - Para inserir um item")
     print("2 - Para inserir vários itens em uma única linha")
-    print("3 - Para inserir em lote por um arquivo")# ficou faltando
-    print("4 - Para imprimir o grau do grafo")
-    print("5 - Para imprimir a ordem do grafo")
-    print("6 - Para imprimir grau e ordem do grafo")
-    print("7 - Para listar os vértices adjacentes")
+    #print("3 - Para inserir em lote por um arquivo")# ficou faltando
+    print("3 - Para imprimir o grau do grafo")
+    print("4 - Para imprimir a ordem do grafo")
+    print("5 - Para imprimir grau e ordem do grafo")
+    print("6 - Para listar os vértices adjacentes")
+    print("7 - Para imprimir o Grau de Adjacência do vertice")
     print("8 - Para identificar se dois vértices são adjacentes")
     print("0 - Para encerrar")
 
@@ -134,23 +135,21 @@ while opcao != 0:
                 flag = False
 
     
-        
-        
     #Imprimindo o grau(req5)
-    if opcao == 4:
+    if opcao == 3:
         print("O grafo possui grau ",len(v))
 
     #Imprimindo a ordem (req5)
-    if opcao == 5:
+    if opcao == 4:
         print("O grafo possui ordem ", len(v1))
 
     # #imprimindo agrau e ordem (req5)
-    if opcao == 6:
+    if opcao == 5:
         print("O grafo possui grau ", len(v))
         print("O grafo possui ordem ", len(v1))
 
     # #Vértices adjacentes(req6)
-    if opcao == 7:
+    if opcao == 6:
         v_exist = 0
         vertice = input("Digite um vértice já inserido no grafo: ")
         j = 0
@@ -186,7 +185,35 @@ while opcao != 0:
                 
             print("\n")
 
-    # #identificando se 2 vértices são adjacentes
+    #imprimindo grau do vértice (req7)
+    if opcao == 7:
+        v_exist = 0
+        vertice = input("Digite um vértice já inserido no grafo: ")
+        j = 0
+        for j in range (len(v)):
+            #print(v[j], "|", vertice, "|")
+            if v[j] == vertice:
+                v_exist = 1
+                break
+        if v_exist == 0:
+            print("Esse vértice ainda não foi inserido")
+        else:
+            adj_entrada = []
+            adj_saida = []
+            i = 0
+            for i in range (len(v1)):
+                if vertice == v1[i]:
+                    adj_saida.append(v2[i])
+                elif vertice == v2[i]:
+                    adj_entrada.append(v1[i])
+
+            if dir == 1:
+                print("Grau de adjacência de entrada: ", len(adj_entrada))
+                print("Grau de adjacência de saída: ", len(adj_saida))
+            elif dir == 0:
+                sum_adj = len(adj_entrada) + len(adj_saida)
+                print("Grau de adjacência: ", sum_adj)
+    # #identificando se 2 vértices são adjacentes (req8)
     if opcao == 8:
         v_exist = 0
         vertice_1 = input("Digite um vértice: ")
